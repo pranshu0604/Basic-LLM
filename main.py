76df76a998018076e9e7b272792b6d0d797ee2b2
Pranshu Pandey <pranshu0604@gmail.com>
2025-05-29 04:38:52 +0530

    add: promptTemplate

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import os
 from dotenv import load_dotenv
 load_dotenv()
-
 from langchain_openai import ChatOpenAI
-from langchain_core.messages import HumanMessage
 from langchain_core.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 
 llm = ChatOpenAI(
@@ -15,11 +13,33 @@ llm = ChatOpenAI(
     callbacks=[StreamingStdOutCallbackHandler()],  # Add callbacks here
 )
 
-# Using streaming with the model
-response = llm.invoke(
-    [HumanMessage(content="Dame una frase ruda en español (Give me a badass phrase in Spanish)")]
-)
 
-# Note: When using streaming, the content is already printed by the handler
-# but you can still access the complete response if needed
-print("\nFull response:", response.content)
+def base():
+    from langchain_core.messages import HumanMessage
+
+
+    # for streaming responses
+    response = llm.invoke(
+        [HumanMessage(content="Dame una frase ruda en español (Give me a badass phrase in Spanish)")]
+    )
+
+    # Note: When using streaming, the content is already printed by the handler
+    # but you can still access the complete response if needed
+    print("\nFull response:", response.content)
+
+
+def promptTemplate():
+    from langchain_core.prompts import ChatPromptTemplate
+
+    system_template = "Translate the following from English to {language}"
+
+    prompt_template = ChatPromptTemplate(
+        [("system", system_template), ("user", "{text}")],
+    )
+
+    prompt = prompt_template.invoke({"language": "Spanish", "text": "Hello, how are you?"})
+
+    response = llm.invoke(prompt)
+    print("\nPrompt response:", response)
+
+promptTemplate()
